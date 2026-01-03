422c929913a14254d998dadd3af3e4544c621697
Aliiel <leila.mouaci@gmail.com>
2026-01-03 20:15:25 +0100

    update repo for exercices

---
+++ .NET/Exercices/Exercice01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Quel est votre prénom ? ");
+string prenomUtilisateur = Console.ReadLine();
+
+Console.WriteLine("Quel est votre nom ? ");
+string nomUtilisateur = Console.ReadLine();
+
+Console.WriteLine($"Bienvenue {prenomUtilisateur} {nomUtilisateur} !!");
+
+Console.WriteLine("Quel est votre âge ? ");
+int ageUtilisateur = Convert.ToInt32(Console.ReadLine());
+
+const int ageRecommande = 21;
+bool accesOk = ageUtilisateur > ageRecommande;
---
+++ README.md
@@ -0,0 +1,3 @@
+### Formation POEC .NET 15/12/25
+
+Repository contenant les exercices 
